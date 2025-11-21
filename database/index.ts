870a2791ddd087b7fdeabc9e39a40b9849e058d0
Irene-IT <irene.it.work@gmail.com>
2025-11-21 12:32:02 +0100

    Implement Database models

---
+++ database/index.ts
@@ -0,0 +1,13 @@
+// import Event from "./event.model";
+// import Booking from "./booking.model";
+
+// export { Event, Booking };
+
+
+// Database models exports
+export { default as Event } from './event.model';
+export { default as Booking } from './booking.model';
+
+// TypeScript interfaces exports
+export type { IEvent } from './event.model';
+export type { IBooking } from './booking.model';
